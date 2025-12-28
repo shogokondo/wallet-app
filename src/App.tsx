515151c5c9ae7a7c2f49d6d4df7562b326611a74
shogokondo <syougo20039534@gmail.com>
2025-12-29 00:25:00 +0900

    レポート画面の追加

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element= {<Home />} />
+        <Route path='/report' element= {<Report />} />
       </Routes>
 
     </Router>
