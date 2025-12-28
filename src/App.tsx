a8d397c8c1cba4e0c5d3938606a27e99a77de845
shogokondo <syougo20039534@gmail.com>
2025-12-29 00:38:59 +0900

    NotFoundページをルーティングに追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Report from './pages/Report';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element= {<Home />} />
+        <Route path='*' element= {<NotFound />} />
       </Routes>
 
     </Router>
