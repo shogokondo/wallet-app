9ced2b7fb8b015d902d1bd6df0cdf3330f763ea1
shogokondo <syougo20039534@gmail.com>
2025-12-29 15:38:57 +0900

    サイドバーコンテンツの追加

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,6 +1,6 @@
 import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import HomeIcon from '@mui/icons-material/Home';
+import { Equalizer } from '@mui/icons-material';
 import React from 'react'
 
 interface SideBarProps {
@@ -10,37 +10,36 @@ interface SideBarProps {
   handleDrawerTransitionEnd: () => void;
 }
 
+interface MenuItem {
+  text: string;
+  path: string; 
+  id: string
+  icon: React.ComponentType;
+}
+
 const SideBar = ({drawerWidth,mobileOpen,handleDrawerClose,handleDrawerTransitionEnd}:SideBarProps) => {
+    const menuItems: MenuItem[] = [
+    { text: 'Home', path: '/', id:"001", icon: HomeIcon },
+    { text: 'Report', path: '/report', id:"001", icon: Equalizer },
+  ];
 
   const drawer = (
     <div>
       <Toolbar />
       <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {menuItems.map((item, index) => (
+          <ListItem key={item.id} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <item.icon />
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={item.text} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
       <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
     </div>
   );
   return (
