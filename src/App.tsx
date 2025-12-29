290b7a5843e939cba62aae6ee80b661cb23dc9a1
shogokondo <syougo20039534@gmail.com>
2025-12-29 10:36:49 +0900

    レイアウトファイルを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import AppLayout from './pages/layout/AppLayout';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element= {<Home />} />
+        <Route path='/' element={<AppLayout />}>
+          <Route path='/' element= {<Home />} />
+        </Route>
         <Route path='*' element= {<NotFound />} />
       </Routes>
 
