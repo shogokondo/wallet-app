d1c4810372dcbed805ef8931cc74652e39ec2e19
shogokondo <syougo20039534@gmail.com>
2025-12-30 15:14:27 +0900

    テーマ設定を追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ import Home from './pages/Home';
 import Report from './pages/Report';
 import NotFound from './pages/NotFound';
 import AppLayout from './components/layout/AppLayout';
-
+import { theme } from './theme/Theme';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<AppLayout />}>
-          <Route path='/' element= {<Home />} />
-          <Route path='/report' element= {<Report />} />
-        </Route>
-        <Route path='*' element= {<NotFound />} />
-      </Routes>
-
-    </Router>
+    <ThemeProvider theme={theme }>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path='/' element={<AppLayout />}>
+            <Route path='/' element= {<Home />} />
+            <Route path='/report' element= {<Report />} />
+          </Route>
+          <Route path='*' element= {<NotFound />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
 
   );
 }
